1d834e0ceec59328d43c70da1e1969f189a17991
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-12-22 10:46:39 +0100

    Fix

--- project3/main.py
+++ project3/main.py
@@ -322,15 +322,15 @@ def train(load_weights, path_weights, resize_w_img, resize_h_img, resize_h_label
         extract_labeled_frames(train_data)
 
         # split data
-        train_data_amateur, train_data_prof, val_data_prof = split_to_get_validation(train_data, split=0.1)
+        train_data_amateur, train_data_prof, val_data_prof = split_to_get_validation(train_data, split=0.05)
 
         # create train of both prof and amateur
         train_data_all = train_data_prof
 
         # Expand with more of the validation data to get a bit more training data. aka now we have 90 % data
-        moreTrainingData, val_data_prof = train_test_split(val_data_prof, test_size=0.66, random_state=18)
+        # moreTrainingData, val_data_prof = train_test_split(val_data_prof, test_size=0.66, random_state=18)
 
-        train_data_all.extend(moreTrainingData)
+        # train_data_all.extend(moreTrainingData)
 
         train_data_all.extend(train_data_all)
         train_data_all.extend(train_data_amateur)
