ff133b02beade678ff3a76063cbe44642dbe17c8
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-12-22 10:45:15 +0100

    Less eval

--- project3/main.py
+++ project3/main.py
@@ -441,15 +441,15 @@ def train_box():
     resize_w_img, resize_h_img = 100, 100
     resize_w_box, resize_h_box = 100, 100
 
-    network_box = train_box(load_weights=False, path_weights='Trained_boxes/signal_unet_smaller/ep-531.pth',
+    network_box = train_box(load_weights=False, path_weights='Trained_boxes/signal_unet_less_eval/ep-531.pth',
                             resize_w_img=resize_w_img, resize_h_img=resize_h_img,
                             resize_w_box=resize_w_box, resize_h_box=resize_h_box)
 
 
 if __name__ == '__main__':
     # train_box()
-    resize_w_img, resize_h_img = 240, 240
-    resize_w_label, resize_h_label = 240, 240
+    resize_w_img, resize_h_img = 400, 400
+    resize_w_label, resize_h_label = 400, 400
     
     network = train(load_weights=False, path_weights='', #'Trained_small_model_512_euler/signal_unet_less_eval/ep-107.pth', 
                     resize_w_img=resize_w_img, resize_h_img=resize_h_img,
